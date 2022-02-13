f701a34ab6a133902d05edd2712368bd915936aa
joiltonrsilva <97770687+joiltonrsilva@users.noreply.github.com>
2022-02-13 20:39:18 -0300

    Update main.py
    Erase comments of the test.

--- main.py
+++ main.py
@@ -5,8 +5,3 @@ from gui.mainui import GeradorUI
 
 if __name__ == '__main__':
     GeradorUI().run()
-    '''print("---- PROGRAMA DE GERADOR DE NUMEROS P/ CÓDIGO DE BARRAS EAN-13 NOS PRODUTOS PESO ----")
-    obj = GeradorCodBarras()
-    digit_value = input("DIGITE O CÓDIGO DO PRODUTO NO SISTEMA:")
-    obj.entry_codproduto = digit_value
-    print("O CÓDIGO DE BARRAS GERADO É:", obj.entry_codproduto)'''
